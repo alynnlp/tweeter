6af95626fb8eebe2233ec22c2068a7f1644fc4f1
Aileen Pang <aileenpang63@gmail.com>
2017-09-22 17:04:30 -0400

    fixed space

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -17,7 +17,6 @@ module.exports = function makeDataHelpers(db) {
         }
       });
     },
-
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       const sortNewestFirst = (a, b) => a.created_at - b.created_at;
